605d3ab036776c94b02474ac91f04cfb85484b8c
arvindswamyniit <swamyarvind63@gmail.com>
2016-08-11 12:07:04 +0530

    Add files via upload

---
+++ Karthouse/src/main/java/Karthouse/controller/HomeController.java
@@ -0,0 +1,14 @@
+package Karthouse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home(){
+		System.out.println("I am in Home Page() method.");
+		return "home";
+	}
+
+}
