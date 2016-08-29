6162b1ce250f83e674a31abecb6953c3e216e675
arvindswamyniit <swamyarvind63@gmail.com>
2016-08-30 00:03:34 +0530

    Add files via upload

---
+++ Carthouse/src/main/java/com/niit/shoppingcart/HomeController.java
@@ -0,0 +1,48 @@
+package com.niit.shoppingcart;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.niit.Yourcart.Dao.CategoryDao;
+import com.niit.Yourcart.model.Category;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	CategoryDao categoryDao;
+	@RequestMapping("/")
+	@Autowired
+	public ModelAndView home()
+	{
+		ModelAndView mv=new ModelAndView("home");
+		mv.addObject("message","thanku for visiting");
+		List<Category> categoryList = categoryDao.list();
+		mv.addObject("categoryList",categoryList);
+		System.out.println("size:"+categoryList.size());
+		return mv;
+	}
+	@RequestMapping("/register")
+	public ModelAndView register()
+	{
+
+		ModelAndView mv=new ModelAndView("register");
+		mv.addObject("user clicked register here","true");
+		return mv;
+	}
+	@RequestMapping("/login")
+	public ModelAndView Login()
+	{
+
+		ModelAndView mv=new ModelAndView("Login");
+		mv.addObject("user clicked register here","true");
+		return mv;
+	}
+	
+	
+
+}
